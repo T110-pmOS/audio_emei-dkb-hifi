8c75616427ca07ae035d339384984bf611cc869e
cheadrian <adry_net2007@yahoo.com>
2023-02-10 19:53:56 +0200

    Create amixer_speaker_off.sh

---
+++ amixer_speaker_off.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+amixer -D sysdefault cset name='PM822_MIS_CLASS_D_2' 48
+amixer -D sysdefault cset name='PM822_MIS_CLASS_D_1' 206
+amixer -D sysdefault cset name='PM822_CLASS_D_1' 0
+amixer -D sysdefault cset name='PM805_CODEC_PDM_SETTING_2' 0
+amixer -D sysdefault cset name='PM805_CODEC_PDM_SETTING_1' 0
+amixer -D sysdefault cset name='PM805_CODEC_VOL_CHANNLE_3_4_SEL' 0
+amixer -D sysdefault cset name='PM805_CODEC_SAI1_SETTING_2' 0
+amixer -D sysdefault cset name='PM805_CODEC_SAI1_SETTING_1' 6
+amixer -D sysdefault cset name='PM805_CODEC_SAI1_SETTING_3' 0
+amixer -D sysdefault cset name='PM805_CODEC_DIGITAL_BLOCK_EN_2' 0
+amixer -D sysdefault cset name='PM805_CODEC_DIGITAL_BLOCK_EN_1' 0
+amixer -D sysdefault cset name='PM805_CODEC_VOL_CTRL_PARAM_SEL' 0
+amixer -D sysdefault cset name='PM805_CODEC_MAIN_POWERUP' 0
