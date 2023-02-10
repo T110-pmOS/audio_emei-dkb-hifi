a9a5429fe335b1baf7bd4e5ee604401fb2761f6e
cheadrian <adry_net2007@yahoo.com>
2023-02-10 19:53:31 +0200

    Create amixer_speaker_on.sh

---
+++ amixer_speaker_on.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+amixer -D sysdefault cset name='PM805_CODEC_MAIN_POWERUP' 3
+amixer -D sysdefault cset name='PM805_CODEC_SAI1_SETTING_2' 73
+amixer -D sysdefault cset name='PM805_CODEC_SAI1_SETTING_1' 14
+amixer -D sysdefault cset name='PM805_CODEC_SAI1_SETTING_3' 4
+amixer -D sysdefault cset name='PM805_CODEC_VOL_CHANNLE_3_4_SEL' 240
+amixer -D sysdefault cset name='PM805_CODEC_DIGITAL_BLOCK_EN_2' 5
+amixer -D sysdefault cset name='PM805_CODEC_MIX_EQ_COEFFICIENT_3' 3
+amixer -D sysdefault cset name='PM805_CODEC_MIX_EQ_COEFFICIENT_3' 3
+amixer -D sysdefault cset name='PM805_CODEC_MIX_EQ_COEFFICIENT_3' 33
+amixer -D sysdefault cset name='PM805_CODEC_DIGITAL_BLOCK_EN_1' 13
+amixer -D sysdefault cset name='PM805_CODEC_VOL_CTRL_PARAM_SEL' 32
+amixer -D sysdefault cset name='PM805_CODEC_PDM_SETTING_2' 65
+amixer -D sysdefault cset name='PM805_CODEC_PDM_SETTING_1' 193
+amixer -D sysdefault cset name='PM822_CLASS_D_1' 242
+amixer -D sysdefault cset name='PM822_MIS_CLASS_D_1' 194
+amixer -D sysdefault cset name='PM822_MIS_CLASS_D_2' 112
